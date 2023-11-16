88b3fc06dfbbd956773082b895aef506977de623
Jefferson <jefferson.brandao@g.globo>
2023-11-15 22:20:35 -0300

    criando filtros de home

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,8 @@
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
 
@@ -24,7 +27,15 @@ const Home = () => {
           placeholder="Pesquisar por perguntas"
           otherClasses="flex-1"
         />
+
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+
+      <HomeFilters />
     </>
   );
 };
---
+++ constants/filters.ts
@@ -0,0 +1,41 @@
+export const AnswerFilters = [
+  { name: "Votos mais positivos", value: "highestUpvotes" },
+  { name: "Votos mais negativos", value: "lowestUpvotes" },
+  { name: "Mais recentes", value: "recent" },
+  { name: "Antigos", value: "old" },
+];
+
+export const UserFilters = [
+  { name: "Novos usuários", value: "new_users" },
+  { name: "Antigos usuários", value: "old_users" },
+  { name: "Top Contribuidores", value: "top_contributors" },
+];
+
+export const QuestionFilters = [
+  { name: "Mais recentes", value: "most_recent" },
+  { name: "Antigos", value: "oldest" },
+  { name: "Mais votados", value: "most_voted" },
+  { name: "Mais vistos", value: "most_viewed" },
+  { name: "Mais respondidas", value: "most_answered" },
+];
+
+export const TagFilters = [
+  { name: "Populares", value: "popular" },
+  { name: "Recentes", value: "recent" },
+  { name: "Nome", value: "name" },
+  { name: "Antigos", value: "old" },
+];
+
+export const HomePageFilters = [
+  { name: "Mais recentes", value: "newest" },
+  { name: "Recomendado", value: "recommended" },
+  { name: "Frequente", value: "frequent" },
+  { name: "Sem resposta", value: "unanswered" },
+];
+
+export const GlobalSearchFilters = [
+  { name: "Pergunta", value: "question" },
+  { name: "Resposta", value: "answer" },
+  { name: "Usuário", value: "user" },
+  { name: "Tag", value: "tag" },
+];
