47716888c7623f2c45b63110c55db5b2767e2437
Jefferson <jefferson.brandao@g.globo>
2023-11-16 22:21:57 -0300

    criando componente de question com os itens restantes

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import React, { useState } from "react";
+import FroalaEditor from "react-froala-wysiwyg";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -16,7 +18,22 @@ import { QuestionsSchema } from "@/lib/validations";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import "froala-editor/js/plugins/image.min.js";
+import "froala-editor/js/plugins/code_beautifier.min.js";
+import "froala-editor/js/plugins/code_view.min.js";
+import "froala-editor/js/plugins/emoticons.min.js";
+import "froala-editor/js/plugins/markdown.min.js";
+import "froala-editor/js/plugins/lists.min.js";
+import "froala-editor/js/plugins/font_size.min.js";
+import { Badge } from "../ui/badge";
+import Image from "next/image";
+
+const type: any = "create";
+
 const Question = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
     defaultValues: {
@@ -26,13 +43,57 @@ const Question = () => {
     },
   });
 
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag deve ter no máximo 15 caracteres",
+          });
+        }
+
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        } else {
+          form.trigger();
+        }
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+
+    form.setValue("tags", newTags);
+  };
+
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
-    console.log(values);
+    setIsSubmitting(true);
+
+    try {
+    } catch (error) {
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
   return (
     <Form {...form}>
-      <form className="flex w-full flex-col gap-10">
+      <form
+        className="flex w-full flex-col gap-10"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
         <FormField
           control={form.control}
           name="title"
@@ -69,7 +130,14 @@ const Question = () => {
                 <span className="text-primary-500">*</span>
               </FormLabel>
 
-              <FormControl className="mt-3.5">{/* todo */}</FormControl>
+              <FormControl className="mt-3.5">
+                <FroalaEditor
+                  tag="textarea"
+                  config={{
+                    height: 300,
+                  }}
+                />
+              </FormControl>
 
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduza o seu problema e explique em detalhes de acordo com o
@@ -91,11 +159,34 @@ const Question = () => {
               </FormLabel>
 
               <FormControl className="mt-3.5">
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  placeholder="Add Tags.."
-                  {...field}
-                />
+                <>
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    placeholder="Add Tags.."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag) => (
+                        <Badge
+                          key={tag}
+                          onClick={() => handleTagRemove(tag, field)}
+                          className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                        >
+                          {tag}{" "}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            width={12}
+                            height={12}
+                            alt="fechar icon"
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
 
               <FormDescription className="body-regular mt-2.5 text-light-500">
@@ -108,8 +199,16 @@ const Question = () => {
           )}
         />
 
-        <Button className="" type="submit">
-          Criar
+        <Button
+          className="primary-gradient w-fit !text-light-900"
+          type="submit"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <>{type === "edit" ? "Editando.." : "Publicando.."}</>
+          ) : (
+            <>{type === "edit" ? "Editar Pergunta" : "Publicar Pergunta"}</>
+          )}
         </Button>
       </form>
     </Form>
