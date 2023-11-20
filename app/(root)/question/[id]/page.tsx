a85390160e9f8cc4114f76da2cf6c8aca579d673
Jefferson <jefferson.brandao@g.globo>
2023-11-19 23:16:54 -0300

    iniciando a contrução da pagina de perguntas

---
+++ app/(root)/question/[id]/page.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import Metric from "@/components/shared/Metric";
+import { getQuestionById } from "@/lib/actions/question.action";
+import { formatAndDivideNumber } from "@/lib/utils";
+import ParseHTML from "@/components/shared/ParseHTML";
+
+const Question = async ({ params }) => {
+  const result = await getQuestionById({ questionId: params.id });
+
+  return (
+    <>
+      <div className="flex-start w-full flex-col">
+        <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+          <Link
+            href={`/profile/${result.author.clerkId}`}
+            className="flex items-center justify-start gap-1"
+          >
+            <Image
+              src={result.author.picture}
+              className="rounded-full"
+              width={22}
+              height={22}
+              alt={`imagem do author: ${result.author.name}`}
+            />
+
+            <p className="paragraph-semibold text-dark300_light700">
+              {result.author.name}
+            </p>
+          </Link>
+
+          <div className="flex justify-end">VOTING</div>
+        </div>
+
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
+          {result.title}
+        </h2>
+      </div>
+
+      <div className="mb-8 mt-5 flex flex-wrap gap-4">
+        <Metric
+          imgUrl="/assets/icons/clock.svg"
+          alt="Perguntado icon"
+          value={formatDistanceToNow(result.createdAt, {
+            addSuffix: true,
+            locale: ptBR,
+          }).replace("em cerca de", "há")}
+          // title=" Perguntado"
+          textStyles="small-medium text-dark400_light800"
+        />
+
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="Menssagens"
+          value={formatAndDivideNumber(result.answers.length)}
+          title=" Respostas"
+          textStyles="small-medium text-dark400_light800"
+        />
+
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(result.views)}
+          title=" Visualizações"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+
+      <ParseHTML data={result.content} />
+    </>
+  );
+};
+
+export default Question;
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache"
 import Question from "@/database/question.model"
 import { connectToDatabase } from "../mongoose"
 import Tag from "@/database/tag.model"
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types"
+import { CreateQuestionParams, GetQuestionByIdParams, GetQuestionsParams } from "./shared.types"
 import User from "@/database/user.model"
 
 
@@ -12,9 +12,9 @@ export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase()
 
-    const questions = await Question.find({}).populate({path: 'tags', model: Tag}).populate({path: 'author', model: User}).sort({createdAt: -1})
+    const questions = await Question.find({}).populate({ path: 'tags', model: Tag }).populate({ path: 'author', model: User }).sort({ createdAt: -1 })
 
-    return {questions}
+    return { questions }
   } catch (error) {
     console.log(error)
     throw error
@@ -48,6 +48,21 @@ export async function createQuestion(params: CreateQuestionParams) {
 
 
     revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function getQuestionById(params: GetQuestionByIdParams) {
+  try {
+    connectToDatabase()
+
+    const { questionId } = params
+
+    const question = await Question.findById(questionId).populate({ path: 'tags', model: Tag, select: '_id name' }).populate({ path: 'author', model: User, select: '_id clerkId name picture' })
+
+    return question
   } catch (error) {
     console.log(error)
     throw error
