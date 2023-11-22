9096ef1fb831209f6a5099bd92ff10413c901f06
Jefferson <jefferson.brandao@g.globo>
2023-11-22 19:04:29 -0300

    configurando o estado do filtro

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,17 +1,47 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "newest";
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (value: string) => {
+    if (active === value) {
+      setActive("");
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(value);
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: value.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
 
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((item) => (
         <Button
           key={item.value}
-          onClick={() => {}}
+          onClickCapture={() => handleTypeClick(item.value)}
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
             active === item.value
               ? "bg-primary-100 text-primary-500"
