546926eb0df400720f9bdc8258e62fb67b1a5577
Jefferson <jefferson.brandao@g.globo>
2023-11-23 14:35:12 -0300

    criando global search filters

--- components/shared/search/GlobalFilters.tsx
+++ components/shared/search/GlobalFilters.tsx
@@ -1,8 +1,59 @@
-import React from 'react'
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { GlobalSearchFilters } from '@/constants/filters'
+import { formUrlQuery } from '@/lib/utils'
+import { useRouter, useSearchParams } from 'next/navigation'
+import React, { useState } from 'react'
 
 const GlobalFilters = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const typeParams = searchParams.get('type')
+
+  const [active, setActive] = useState(typeParams || '')
+
+  const handleTypeClick = (value: string) => {
+    if (active === value) {
+      setActive("");
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(value);
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: value.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
-    <div>GlobalFilters</div>
+    <div className='flex items-center gap-5 px-5'>
+      <p className='text-dark400_light900 body-medium'>Type: </p>
+
+      <div className='flex gap-3'>
+        {GlobalSearchFilters.map((item) => (
+          <Button 
+          key={item.value}
+          onClick={() => handleTypeClick(item.value)} 
+          type="button" 
+          className={`light-border-2 small-medium rounded-2xl px-5 py-2 capitalize dark:text-light-800 dark:hover:text-primary-500 ${active === item.value ? "bg-primary-500 text-light-500" : "bg-light-700 text-dark-500 hover:text-primary-500 dark:bg-dark-500"}`}>
+            {item.name}
+          </Button>
+        ))}
+      </div>
+    </div>
   )
 }
 
