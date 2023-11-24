04cc74db1ace4e590292a57c093d1a533256b44b
Jefferson <jefferson.brandao@g.globo>
2023-11-23 21:15:52 -0300

    implementando reputation points para perguntas

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -88,6 +88,14 @@ export async function createQuestion(params: CreateQuestionParams) {
 
     await Question.findByIdAndUpdate(question._id, { $push: { tags: { $each: tagDocuments } } })
 
+    await Interaction.create({
+      user: author,
+      action: "ask_question",
+      question: question._id,
+      tags: tagDocuments,
+    })
+
+    await User.findByIdAndUpdate(author, { $inc: { reputation: 5 } })
 
     revalidatePath(path)
   } catch (error) {
@@ -132,6 +140,10 @@ export async function upvoteQuestion(params: QuestionVoteParams) {
     const question = await Question.findByIdAndUpdate(questionId, updateQuery, { new: true })
 
     if (!question) throw new Error('Question not found')
+
+    await User.findByIdAndUpdate(userId, { $inc: { reputation: hasupVoted ? -1 : 1 } })
+
+    await User.findByIdAndUpdate(question.author, { $inc: { reputation: hasupVoted ? -10 : 10 } })
 
     revalidatePath(path)
   } catch (error) {
